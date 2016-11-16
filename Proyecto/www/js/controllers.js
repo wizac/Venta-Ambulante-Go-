31e4fbe3b08b1587440711557f98bfc0fe7b85fb
Damian Villarreal <Damian Villarreal>
2016-11-16 14:52:52 -0300

    pela

--- Proyecto/www/js/controllers.js
+++ Proyecto/www/js/controllers.js
@@ -1,6 +1,5 @@
 var productos = [];
 angular.module('app.controllers', [])
-  
 .controller('misDatosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
