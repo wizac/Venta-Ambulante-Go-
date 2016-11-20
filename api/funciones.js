9b59109967f04b2afd53247116e6b3f96b91ee7f
ggpq22 <ggpq13@hotmail.com>
2016-11-20 00:35:03 -0300

    gonzalo

--- api/funciones.js
+++ api/funciones.js
@@ -1,11 +1,43 @@
-function insertar(db)
+function ventasOrdenadas(db)
 {
 	return function(req, res) {
 		
 		var dbEntidad = db.get(req.params.entidad);
-		console.log(req.body);
-		dbEntidad.insert(req.body);
-		res.send({success: true, message: 'Se inserto correctamente!'});
+		dbEntidad.find(req.body,function(err,docs){
+			if(err){
+				res.send({ success: false, message: 'no se pudo listar la entidad, o no existe' });
+			}
+			else{
+				res.send({
+						success: true,
+						message: 'Lista de entidad exitosa!',
+						res:docs
+						});
+				console.log(docs);
+			}
+		});
+	}
+}
+
+
+function pedidosOrdenados(db)
+{
+	return function(req, res) {
+		
+		var dbEntidad = db.get(req.params.entidad);
+		dbEntidad.find(req.body,function(err,docs){
+			if(err){
+				res.send({ success: false, message: 'no se pudo listar la entidad, o no existe' });
+			}
+			else{
+				res.send({
+						success: true,
+						message: 'Lista de entidad exitosa!',
+						res:docs
+						});
+				console.log(docs);
+			}
+		});
 	}
 }
 
